673f884785dde6c46292cce98125e5ceaca696b7
longatroniks <karlolongin13@gmail.com>
2024-11-07 19:48:28 +0100

    Implement new logic and design of IntroActivity

---
+++ app/src/main/java/dte/masteriot/mdp/asteroidconspiracist/activities/IntroActivity.java
@@ -0,0 +1,27 @@
+package dte.masteriot.mdp.asteroidconspiracist.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import dte.masteriot.mdp.asteroidconspiracist.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, MenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+}
